c9ad398a39f018b92907fc1f7e9524bc59adbbaa
juancarlosSH <juancasu_900@hotmail.com>
2024-11-25 23:29:58 -0600

    Add: hello world and comments.

---
+++ mouredev/00-hello-world.js
@@ -0,0 +1,9 @@
+// This is comment in single line
+
+/**
+ * This is a comment in muiltiple lines
+ */
+
+console.log("Hello Javascript!");
+console.log("Hello Javascript!");
+console.log(`Hello Javascript!`);
