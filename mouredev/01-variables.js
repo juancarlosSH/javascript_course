07e8ed4f1a2f75c98f3a6a2a9d396ce1973e9ca4
juancarlosSH <juancasu_900@hotmail.com>
2024-11-25 23:30:33 -0600

    Add: variables lesson.

---
+++ mouredev/01-variables.js
@@ -0,0 +1,14 @@
+// var
+
+var helloWorld = "Hello Javascript!";
+console.log(helloWorld);
+
+// let
+
+let helloWorld2 = "Hello Javascript!";
+console.log(helloWorld2);
+
+// const
+
+const helloWorld3 = "Hello Javaacript!";
+console.log(helloWorld3);
