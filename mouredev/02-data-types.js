471d6515f20d0b96b6ef8c8a566406f43f219730
juancarlosSH <juancasu_900@hotmail.com>
2024-11-28 20:46:12 -0600

    Add: Datatypes lesson.

---
+++ mouredev/02-data-types.js
@@ -0,0 +1,32 @@
+//Primitive data types
+
+// string
+let myName = 'Juan Carlos!';
+
+// integer
+let age = 25;
+
+// boolean
+let isStudent = true;
+let isAnAnimal = false;
+
+// undefined
+let myFavoriteClass;
+console.log(myFavoriteClass);
+
+// null
+let nullValue = null;
+
+// symbol
+let mySymbol = Symbol('mySymbol');
+
+// BigInt
+let bigInt = BigInt(8437558937589378957374875837894578347895734);
+
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof myFavoriteClass);
+console.log(typeof nullValue);
+console.log(typeof mySymbol);
+console.log(typeof bigInt);
