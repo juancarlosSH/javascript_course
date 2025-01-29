d146df9f7868c7439339a1c95d5724275b41ce9f
juancarlosSH <juancasu_900@hotmail.com>
2025-01-28 20:55:28 -0600

    Add Lesson 1 for the advanced course.

---
+++ bluuweb/advanced_course/lesson_1/project_1/src/main.js
@@ -0,0 +1,7 @@
+// Install nodejs and npm in version 18.19.1 and 9.2.0.
+// Use the comnmand npm create vite@latest to create a new project.
+// Put a name and select the optinon vanilla for JavaScript.
+// Go to the project folder and open the terminal.
+// Run the command npm i to install the dependencies.
+// Run the command npm run dev to start the server.
+console.log("Hello World");
